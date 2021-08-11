72143d56efeaeb3718054e9ae4a336bad6a11cd3
AzizLabidi1 <mohamedaziz.labidi@medtech.tn>
2021-08-11 09:56:59 +0100

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+import clouds from "./imageInSrc.jpg"
+import './style.css';
+import ta9chira from "./ta9chira.mp4"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+<div style={{border: 'solid 2px black ', maxWidth: '100vw '}}>
+<h1 className="title red">Mohamed Aziz Labidi</h1>
+<br></br>
+<img style= {{ width: 800 , margin: '10%' }} src={clouds} alt="cloudsimage" />
+<br></br>
+<img style={{ width: 800 , margin: '10%'}} src="/imageInPublic.jpg" />
+</div>
+<video width={500} height={350} controls>
+<source src={ta9chira} type="video/mp4" />
+</video>
+</div>
   );
 }
 
